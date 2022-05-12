bd1f1f97dc102c44b7bad0d0503cc3f3a94d12b2
NyergesC <cintia.nyerges@gmail.com>
2022-05-12 20:38:58 +0200

    half

--- reactinitial/src/components/Subscription.jsx
+++ reactinitial/src/components/Subscription.jsx
@@ -1,21 +1,19 @@
 import React from 'react'
 
-
 function Subscription() {
- 
 
- 
-  return (
-    <div>
-        <form>
-            <h2>Subscribe to our newsletter</h2>
-            <input type='email' name="email"/>
-            <input type='submit' name="submit" value='submit' pattern=".+@globex\.com" size="30" required/>
-
-        </form>
-        <></>
-    </div>
-  )
-}
+    return (
+        <div>
+            <form action="https://demoapi.com/api/series/newsletter" method='POST'>
+                <h2>Subscribe to our newsletter</h2>
+                <label>
+                    Enter your email:
+                    <input type="email" name="email" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,}$" required/>
+                </label>
+                <input type="submit" value="Submit" />
+            </form>
+        </div>
+    );
+};
 
 export default Subscription
